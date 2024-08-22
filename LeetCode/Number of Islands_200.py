bd572edc1a652fd5978abece21ed087f48d32eec
suuding <95485737+suuding@users.noreply.github.com>
2024-08-22 23:36:46 +0900

    Update and rename [LTC]Number of Islands_200.py to Number of Islands_200.py

--- LeetCode/Number of Islands_200.py
+++ LeetCode/Number of Islands_200.py
@@ -24,3 +24,26 @@ class Solution:
                     dfs(i, j)
                     cnt+=1
         return cnt
+
+############################################
+# 재풀이 
+# DFS
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y) :
+            di = [[0,1],[1,0],[-1,0],[0,-1]]
+            grid[x][y] = '0'
+            for d in di :
+                dx, dy = x+d[0], y+d[1]
+                if 0<=dx<len(grid) and 0<=dy<len(grid[0]) and grid[dx][dy]=='1' :
+                    dfs(dx, dy)
+            return
+        
+        ans = 0
+        for i in range(len(grid)) :
+            for j in range(len(grid[0])) :
+                if grid[i][j]=='1' :
+                    dfs(i, j)
+                    ans += 1
+        return ans
